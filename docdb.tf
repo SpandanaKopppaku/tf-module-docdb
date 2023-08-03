7cceac9cd134264e574e332e918decf64af5829b
spandanakoppaku <skoppaku@gmail.com>
2023-08-03 13:21:56 -0400

    Auto Push

--- docdb.tf
+++ docdb.tf
@@ -3,5 +3,21 @@ resource "aws_docdb_cluster" "docdb" {
   engine                    = "docdb"
   master_username           = "admin1"
   master_password           = "roboshop1"
-  skip_final_snapshot       = true  
+  skip_final_snapshot       = true 
+  vpc_security_group_ids    = ? 
+  db_subnet_group_name      = ? 
+
+#   backup_retention_period   = 14
+#   preferred_backup_window   = "02:00-05:00"
+}
+
+# Creates subnet group
+
+resource "aws_docdb_subnet_group" "docdb" {
+  name       = "main"
+  subnet_ids = [aws_subnet.frontend.id, aws_subnet.backend.id]
+
+  tags = {
+    Name = "My docdb subnet group"
+  }
 }
