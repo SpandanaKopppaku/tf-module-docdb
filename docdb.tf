3c21debda7c2b2d1db66637022ca1572cdc4a153
spandanakoppaku <skoppaku@gmail.com>
2023-08-07 10:49:37 -0400

    Auto Push

--- docdb.tf
+++ docdb.tf
@@ -6,6 +6,7 @@ resource "aws_docdb_cluster" "docdb" {
   skip_final_snapshot       = true 
   vpc_security_group_ids    = [aws_security_group.allows_docbd.id]
   db_subnet_group_name      = aws_docdb_subnet_group.aws_docdb_subnet_group.name
+  
 
 #   backup_retention_period   = 14
 #   preferred_backup_window   = "02:00-05:00"
