011380a7464e48aa85690025ec4fac93556611f6
spandanakoppaku <skoppaku@gmail.com>
2023-08-07 16:50:47 -0400

    Auto Push

--- docdb.tf
+++ docdb.tf
@@ -6,7 +6,7 @@ resource "aws_docdb_cluster" "docdb" {
   skip_final_snapshot       = true 
   vpc_security_group_ids    = [aws_security_group.allows_docbd.id]
   db_subnet_group_name      = aws_docdb_subnet_group.aws_docdb_subnet_group.name
-  
+
 
 #   backup_retention_period   = 14
 #   preferred_backup_window   = "02:00-05:00"
@@ -28,7 +28,7 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   count              = 1
   identifier         = "roboshop-docdb-${var.ENV}-instance"
   cluster_identifier = aws_docdb_cluster.docdb.id       # This argumnet attaches the nodes created here to the docdb cluster
-  instance_class     = "db.t3.medium"
+  instance_class     = var.DOCDB_INSTANCE_TYPE
 
   depends_on         = [aws_docdb_cluster.docdb]
 }
